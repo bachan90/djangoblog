636b9f43957b715374f3d6cbd54b760c0ae94fd0
Michał <bachanowicz.m@gmail.com>
2023-11-19 10:18:21 +0100

    Zmiana debug = False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mlj=^6$=13y5yr2ny7t098+ieinb=+2=*pu#d=p298yfzloou+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com','bachan.pythonanywhere.com']
 
